56f4cd89f2bd98b9bc0fc3aa9c2738ef98154771
unknown <muhammadfarhan3120@gmail.com>
2024-06-19 23:17:39 +0530

    Changes

--- src/Pages/Components/YouTubePlayer.jsx
+++ src/Pages/Components/YouTubePlayer.jsx
@@ -1,18 +1,33 @@
+import React, { useState } from 'react';
 import YouTube from 'react-youtube';
-import '../Styles/YouTubePlayer.css'
+import '../Styles/YouTubePlayer.css';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const YouTubePlayer = ({ videoId }) => {
-    const opts = {
-      playerVars: {
-        autoplay: 0,
-      },
-    };
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <div className="video-responsive">
-          <YouTube videoId={videoId} opts={opts} />
-        </div>
-      );
+  const opts = {
+    playerVars: {
+      autoplay: 0,
+    },
   };
 
-  export default YouTubePlayer
+  const onReady = (event) => {
+    setLoading(false);
+  };
+
+  return (
+    <div className="video-container">
+      {loading && (
+        <div className="spinner-container">
+          <ClipLoader color="#df1827" loading={loading} size={50} />
+        </div>
+      )}
+      <div className={`video-responsive ${loading ? 'hidden' : ''}`}>
+        <YouTube videoId={videoId} opts={opts} onReady={onReady} />
+      </div>
+    </div>
+  );
+};
+
+export default YouTubePlayer;

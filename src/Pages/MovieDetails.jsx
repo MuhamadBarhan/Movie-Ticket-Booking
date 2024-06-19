a86d702541706ceebe078b6a4dcd3840db8914ed
unknown <muhammadfarhan3120@gmail.com>
2024-06-19 20:00:35 +0530

    Changes

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { movies_recommended , movies_fun } from '../assets/Movies';
+import { movies_recommended, movies_fun } from '../assets/Movies';
 import YouTubePlayer from './Components/YouTubePlayer';
+import './Styles/MovieDetails.css'
 
 const MovieDetails = () => {
 
   const params = useParams();
   const mergedMovies = [...movies_recommended, ...movies_fun];
-  const movie = mergedMovies.find((element)=> element.id===parseInt(params.id));
+  const movie = mergedMovies.find((element) => element.id === parseInt(params.id));
 
   return (
-    <div>
-      <p style={{fontSize:'20px'}}>{movie.title}</p>
+    <div className='movie-details'>
       <YouTubePlayer videoId={movie.videoId} />
+      <p className='movie-heading'>{movie.title}</p>
+      <p className='movie-about'>{movie.description}</p>
+      <p className='movie-heading'>Cast</p>
+      <div className="cast-container">
+      {movie.cast.map((movieCast, index) => (
+        <div className="cast-item" key={index}>
+          <img src={movieCast.link} alt={movieCast.name} className='cast-image'/>
+          <p>{movieCast.name}</p>
+        </div>
+      ))}
+      </div>
     </div>
   )
 }
--- src/assets/Movies.jsx
+++ src/assets/Movies.jsx
@@ -1,26 +1,236 @@
-export  const images = [
-    'https://assets-in.bmscdn.com/discovery-catalog/events/et00055008-txnsnrpgpg-landscape.jpg',
-    'https://cdna.artstation.com/p/assets/images/images/025/095/038/large/almu-redondo-aladdin-banner-horizontal.jpg?1584597967',
-    'https://w0.peakpx.com/wallpaper/204/388/HD-wallpaper-movie-frozen.jpg',
-    'https://assets-in.bmscdn.com/discovery-catalog/events/et00006168-ylzbrzeduv-landscape.jpg',
-    'https://c4.wallpaperflare.com/wallpaper/943/868/835/ice-age-4-continental-drift-2012-ice-age-4-movie-poster-wallpaper-preview.jpg',
-    'https://i0.wp.com/www.heyuguys.com/images/2012/07/Ice-Age-Continental-Drift-poster-2.jpg?ssl=1',
-    'https://fifthsun.com/cdn/shop/articles/5sun_lion_king_blog_1024x.jpg?v=1603994032',
-    'https://i.ebayimg.com/images/g/VNwAAOSwu3lkuGJI/s-l1600.jpg',
-  ];
+export const images = [
+  'https://assets-in.bmscdn.com/discovery-catalog/events/et00055008-txnsnrpgpg-landscape.jpg',
+  'https://cdna.artstation.com/p/assets/images/images/025/095/038/large/almu-redondo-aladdin-banner-horizontal.jpg?1584597967',
+  'https://w0.peakpx.com/wallpaper/204/388/HD-wallpaper-movie-frozen.jpg',
+  'https://assets-in.bmscdn.com/discovery-catalog/events/et00006168-ylzbrzeduv-landscape.jpg',
+  'https://c4.wallpaperflare.com/wallpaper/943/868/835/ice-age-4-continental-drift-2012-ice-age-4-movie-poster-wallpaper-preview.jpg',
+  'https://i0.wp.com/www.heyuguys.com/images/2012/07/Ice-Age-Continental-Drift-poster-2.jpg?ssl=1',
+  'https://fifthsun.com/cdn/shop/articles/5sun_lion_king_blog_1024x.jpg?v=1603994032',
+  'https://i.ebayimg.com/images/g/VNwAAOSwu3lkuGJI/s-l1600.jpg',
+];
 
 export const movies_recommended = [
-    { id: 1, link: 'https://media1.popsugar-assets.com/files/thumbor/jUZ7dw2WSsWsEZxvwA-O6IHiumQ=/1688x2500/filters:format_auto():quality(85):extract_cover()/2017/01/26/813/n/1922283/055dc333c3280d59_BeautyAndTheBeast58726d5b9fac8.jpg', title: 'Beauty and The Beast' , videoId:'e3Nl_TCQXuw'},
-    { id: 2, link: 'https://lumiere-a.akamaihd.net/v1/images/p_ratatouille_19736_0814231f.jpeg', title: 'Ratatouille ' , videoId:'NgsQ8mVkN8w'},
-    { id: 3, link: 'https://images-cdn.ubuy.co.in/63599fbd272b6e5380399653-poster-stop-online-frozen-disney-movie.jpg', title: 'Frozen' , videoId:'TbQm5doF_Uc'},
-    { id: 4, link: 'https://images-cdn.ubuy.co.in/63515cf508fd08526949d373-toy-story-disney-pixar-movie-poster.jpg', title: 'Toy Story' , videoId:'v-PjgYDrg70'},
-    { id: 5, link: 'https://i.ebayimg.com/images/g/z0IAAOSwImRYRDmY/s-l1600.jpg', title: 'Tangled' , videoId:'ycoY201RTRo'},
-  ];
+  {
+    id: 1,
+    link: 'https://media1.popsugar-assets.com/files/thumbor/jUZ7dw2WSsWsEZxvwA-O6IHiumQ=/1688x2500/filters:format_auto():quality(85):extract_cover()/2017/01/26/813/n/1922283/055dc333c3280d59_BeautyAndTheBeast58726d5b9fac8.jpg',
+    title: 'Beauty and The Beast',
+    videoId: 'e3Nl_TCQXuw',
+    cast: [
+      {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLI_ZUEAgFfjSklH-5L9m_nFRnC_JATNuj5I6Lj1vNg2NV7LDWaWSbBNU&s=10',
+        name: 'Emma Watson'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQGANTrvzSOdJeIc79d76f58FUJR1OGiKGI-hVXdB-BUrfcf6oqh87KrpSfZzf6p5LRttbPVxc&s=19',
+        name: 'Dan Stevens'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_p51WFmMem-C2Tg__NTNbBYjNWrLf9sveP6oBuS4vQKppCZRtVv97ySk&s=10',
+        name: 'Luke Evans'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNfBSlAWPSprptF-fHJ8B_fQkorKD_yx1EcuFS1OgxdgMYdiI-N-cazg&s=10',
+        name: 'Josh Gad'
+      },
+    ],
+    description: 'Belle embarks on a journey to save her father from a creature that has locked him in his dungeon. Eventually, she learns that the beast is an enchanted prince who has been cursed.'
+  },
+  {
+    id: 2,
+    link: 'https://lumiere-a.akamaihd.net/v1/images/p_ratatouille_19736_0814231f.jpeg',
+    title: 'Ratatouille ',
+    videoId: 'NgsQ8mVkN8w',
+    cast: [
+      {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLI_ZUEAgFfjSklH-5L9m_nFRnC_JATNuj5I6Lj1vNg2NV7LDWaWSbBNU&s=10',
+        name: 'Emma Watson'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQGANTrvzSOdJeIc79d76f58FUJR1OGiKGI-hVXdB-BUrfcf6oqh87KrpSfZzf6p5LRttbPVxc&s=19',
+        name: 'Dan Stevens'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_p51WFmMem-C2Tg__NTNbBYjNWrLf9sveP6oBuS4vQKppCZRtVv97ySk&s=10',
+        name: 'Luke Evans'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNfBSlAWPSprptF-fHJ8B_fQkorKD_yx1EcuFS1OgxdgMYdiI-N-cazg&s=10',
+        name: 'Josh Gad'
+      },
+    ],
+    description: 'Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.'
+  },
+  {
+    id: 3,
+    link: 'https://images-cdn.ubuy.co.in/63599fbd272b6e5380399653-poster-stop-online-frozen-disney-movie.jpg',
+    title: 'Frozen',
+    videoId: 'TbQm5doF_Uc',
+    cast: [
+      {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLI_ZUEAgFfjSklH-5L9m_nFRnC_JATNuj5I6Lj1vNg2NV7LDWaWSbBNU&s=10',
+        name: 'Emma Watson'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQGANTrvzSOdJeIc79d76f58FUJR1OGiKGI-hVXdB-BUrfcf6oqh87KrpSfZzf6p5LRttbPVxc&s=19',
+        name: 'Dan Stevens'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_p51WFmMem-C2Tg__NTNbBYjNWrLf9sveP6oBuS4vQKppCZRtVv97ySk&s=10',
+        name: 'Luke Evans'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNfBSlAWPSprptF-fHJ8B_fQkorKD_yx1EcuFS1OgxdgMYdiI-N-cazg&s=10',
+        name: 'Josh Gad'
+      },
+    ],
+    description: 'Snow Queen Elsa inadvertently uses her power to make her kingdom experience constant wintriness. Her sister Anna teams up with mountaineer Kristoff and his reindeer to demolish the snowy spell.'
+  },
+  {
+    id: 4,
+    link: 'https://images-cdn.ubuy.co.in/63515cf508fd08526949d373-toy-story-disney-pixar-movie-poster.jpg',
+    title: 'Toy Story',
+    videoId: 'v-PjgYDrg70',
+    cast: [
+      {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLI_ZUEAgFfjSklH-5L9m_nFRnC_JATNuj5I6Lj1vNg2NV7LDWaWSbBNU&s=10',
+        name: 'Emma Watson'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQGANTrvzSOdJeIc79d76f58FUJR1OGiKGI-hVXdB-BUrfcf6oqh87KrpSfZzf6p5LRttbPVxc&s=19',
+        name: 'Dan Stevens'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_p51WFmMem-C2Tg__NTNbBYjNWrLf9sveP6oBuS4vQKppCZRtVv97ySk&s=10',
+        name: 'Luke Evans'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNfBSlAWPSprptF-fHJ8B_fQkorKD_yx1EcuFS1OgxdgMYdiI-N-cazg&s=10',
+        name: 'Josh Gad'
+      },
+    ],
+    description: 'Andy\'s favourite toy, Woody, is worried that after Andy receives his birthday gift, a new toy called Buzz Lightyear, his importance may get reduced. He thus hatches a plan to eliminate Buzz.'
+  },
+  {
+    id: 5,
+    link: 'https://i.ebayimg.com/images/g/z0IAAOSwImRYRDmY/s-l1600.jpg',
+    title: 'Tangled',
+    videoId: 'ycoY201RTRo',
+    cast: [
+      {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLI_ZUEAgFfjSklH-5L9m_nFRnC_JATNuj5I6Lj1vNg2NV7LDWaWSbBNU&s=10',
+        name: 'Emma Watson'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQGANTrvzSOdJeIc79d76f58FUJR1OGiKGI-hVXdB-BUrfcf6oqh87KrpSfZzf6p5LRttbPVxc&s=19',
+        name: 'Dan Stevens'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_p51WFmMem-C2Tg__NTNbBYjNWrLf9sveP6oBuS4vQKppCZRtVv97ySk&s=10',
+        name: 'Luke Evans'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNfBSlAWPSprptF-fHJ8B_fQkorKD_yx1EcuFS1OgxdgMYdiI-N-cazg&s=10',
+        name: 'Josh Gad'
+      },
+    ],
+    description: 'Rapunzel, an innocent, young girl, is locked up by her overly protective mother. Her wish to escape into the world outside finally comes true when she meets the good-hearted thief, Flynn.'
+  },
+];
 
 export const movies_fun = [
-    { id: 6, link: 'https://lumiere-a.akamaihd.net/v1/images/p_funandfancyfree_19641_233db24d.jpeg', title: 'Fun and Fancy Free' , videoId:'-q4CvXRWc4s'},
-    { id: 7, link: 'https://lumiere-a.akamaihd.net/v1/images/p_monstersinc_19751_55afa07a.jpeg', title: 'Monsters,Inc. ' , videoId:'6tCxnHCqqxg'},
-    { id: 8, link: 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p25089179_v_v13_aa.jpg', title: 'The Slumber Party' , videoId:'cN8iIKQvI_U'},
-    { id: 9, link: 'https://m.media-amazon.com/images/M/MV5BMTY5OTU0OTc2NV5BMl5BanBnXkFtZTcwMzU4MDcyMQ@@._V1_.jpg', title: 'The Incredibles' , videoId:'-UaGUdNJdRQ'},
-    { id: 10, link: 'https://gd120jmaiorano.weebly.com/uploads/1/3/7/5/13751403/1242113.png?470', title: 'Home Alone 2' , videoId:'5h9VDUNtoto'},
-  ];
+  {
+    id: 6,
+    link: 'https://lumiere-a.akamaihd.net/v1/images/p_funandfancyfree_19641_233db24d.jpeg',
+    title: 'Fun and Fancy Free',
+    videoId: '-q4CvXRWc4s',
+    cast: [
+      {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLI_ZUEAgFfjSklH-5L9m_nFRnC_JATNuj5I6Lj1vNg2NV7LDWaWSbBNU&s=10',
+        name: 'Emma Watson'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQGANTrvzSOdJeIc79d76f58FUJR1OGiKGI-hVXdB-BUrfcf6oqh87KrpSfZzf6p5LRttbPVxc&s=19',
+        name: 'Dan Stevens'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_p51WFmMem-C2Tg__NTNbBYjNWrLf9sveP6oBuS4vQKppCZRtVv97ySk&s=10',
+        name: 'Luke Evans'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNfBSlAWPSprptF-fHJ8B_fQkorKD_yx1EcuFS1OgxdgMYdiI-N-cazg&s=10',
+        name: 'Josh Gad'
+      },
+    ],
+    description: 'Ventriloquist Edgar Bergen introduces Disney classic cartoons: Bongo, about a unicycling circus bear, and `Mickey and the Beanstalk. Fun and Fancy Free is a delightful gem that not only sparkles with charm, but is unbelievably rich in history-making Disney moments.'
+  },
+  {
+    id: 7,
+    link: 'https://lumiere-a.akamaihd.net/v1/images/p_monstersinc_19751_55afa07a.jpeg',
+    title: 'Monsters,Inc. ',
+    videoId: '6tCxnHCqqxg',
+    cast: [
+      {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLI_ZUEAgFfjSklH-5L9m_nFRnC_JATNuj5I6Lj1vNg2NV7LDWaWSbBNU&s=10',
+        name: 'Emma Watson'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQGANTrvzSOdJeIc79d76f58FUJR1OGiKGI-hVXdB-BUrfcf6oqh87KrpSfZzf6p5LRttbPVxc&s=19',
+        name: 'Dan Stevens'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_p51WFmMem-C2Tg__NTNbBYjNWrLf9sveP6oBuS4vQKppCZRtVv97ySk&s=10',
+        name: 'Luke Evans'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNfBSlAWPSprptF-fHJ8B_fQkorKD_yx1EcuFS1OgxdgMYdiI-N-cazg&s=10',
+        name: 'Josh Gad'
+      },
+    ],
+    description: 'In a world of monsters, best friends Sulley and Mike work for an energy company which requires them to scare children. However, their lives are hugely disrupted by the appearance of a human girl.'
+  },
+  {
+    id: 8,
+    link: 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p25089179_v_v13_aa.jpg',
+    title: 'The Slumber Party',
+    videoId: 'cN8iIKQvI_U',
+    cast: [
+      {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLI_ZUEAgFfjSklH-5L9m_nFRnC_JATNuj5I6Lj1vNg2NV7LDWaWSbBNU&s=10',
+        name: 'Emma Watson'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQGANTrvzSOdJeIc79d76f58FUJR1OGiKGI-hVXdB-BUrfcf6oqh87KrpSfZzf6p5LRttbPVxc&s=19',
+        name: 'Dan Stevens'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_p51WFmMem-C2Tg__NTNbBYjNWrLf9sveP6oBuS4vQKppCZRtVv97ySk&s=10',
+        name: 'Luke Evans'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNfBSlAWPSprptF-fHJ8B_fQkorKD_yx1EcuFS1OgxdgMYdiI-N-cazg&s=10',
+        name: 'Josh Gad'
+      },
+    ],
+    description: 'Depicting the aftermath of a sleepover birthday party hypnotism gone wrong as best friends Megan and Paige, along with soon-to-be step-sister Veronica, wake up with absolutely no memory of the night before.'
+  },
+  {
+    id: 9,
+    link: 'https://m.media-amazon.com/images/M/MV5BMTY5OTU0OTc2NV5BMl5BanBnXkFtZTcwMzU4MDcyMQ@@._V1_.jpg',
+    title: 'The Incredibles',
+    videoId: '-UaGUdNJdRQ',
+    cast: [
+      {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLI_ZUEAgFfjSklH-5L9m_nFRnC_JATNuj5I6Lj1vNg2NV7LDWaWSbBNU&s=10',
+        name: 'Emma Watson'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQGANTrvzSOdJeIc79d76f58FUJR1OGiKGI-hVXdB-BUrfcf6oqh87KrpSfZzf6p5LRttbPVxc&s=19',
+        name: 'Dan Stevens'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_p51WFmMem-C2Tg__NTNbBYjNWrLf9sveP6oBuS4vQKppCZRtVv97ySk&s=10',
+        name: 'Luke Evans'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNfBSlAWPSprptF-fHJ8B_fQkorKD_yx1EcuFS1OgxdgMYdiI-N-cazg&s=10',
+        name: 'Josh Gad'
+      },
+    ],
+    description: 'Forced to adopt a civilian identity and stuck in a white-collar job, Mr Incredible itches to get back into action. When he is lured into a trap by the evil Syndrome, his family contrives to save him.'
+  },
+  {
+    id: 10,
+    link: 'https://gd120jmaiorano.weebly.com/uploads/1/3/7/5/13751403/1242113.png?470',
+    title: 'Home Alone 2',
+    videoId: '5h9VDUNtoto',
+    cast: [
+      {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLI_ZUEAgFfjSklH-5L9m_nFRnC_JATNuj5I6Lj1vNg2NV7LDWaWSbBNU&s=10',
+        name: 'Emma Watson'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQGANTrvzSOdJeIc79d76f58FUJR1OGiKGI-hVXdB-BUrfcf6oqh87KrpSfZzf6p5LRttbPVxc&s=19',
+        name: 'Dan Stevens'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_p51WFmMem-C2Tg__NTNbBYjNWrLf9sveP6oBuS4vQKppCZRtVv97ySk&s=10',
+        name: 'Luke Evans'
+      }, {
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJNfBSlAWPSprptF-fHJ8B_fQkorKD_yx1EcuFS1OgxdgMYdiI-N-cazg&s=10',
+        name: 'Josh Gad'
+      },
+    ],
+    description: 'Kevin accidentally boards a flight to New York City and gets separated from his family who are on their way to Miami. He then bumps into two of his old enemies, who plan to rob a toy store.'
+  },
+];

2d9e58f7bb45fd70590f496d9130caaa003c06d9
unknown <muhammadfarhan3120@gmail.com>
2024-06-19 22:47:24 +0530

    Changes

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { movies_recommended, movies_fun } from '../assets/Movies';
 import YouTubePlayer from './Components/YouTubePlayer';
 import './Styles/MovieDetails.css'
@@ -14,6 +16,8 @@ const MovieDetails = () => {
     <div className='movie-details'>
       <YouTubePlayer videoId={movie.videoId} />
       <p className='movie-heading'>{movie.title}</p>
+      <p className='movie-rating'><FontAwesomeIcon icon={faStar} /> 8.5/10</p>
+      <span className='movie-type'>2D</span><span className='movie-type'>English</span>
       <p className='movie-about'>{movie.description}</p>
       <p className='movie-heading'>Cast</p>
       <div className="cast-container">
